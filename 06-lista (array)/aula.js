97d125edb77792c9da00e5447a2032b7246c7727
Binhotti <vitorbinhotti@gmail.com>
2026-03-20 21:16:02 -0300

    revisando arrays (listas)

---
+++ 06-lista (array)/aula.js
@@ -0,0 +1,18 @@
+// const alunos = ['João', 'Vitor', 'Marina'];
+
+// alunos.push('Renan');
+
+// alunos[4] = 'Vinicius';
+
+// console.log(alunos);
+
+
+const notas = [];
+
+notas.push(5);
+notas.push(7);
+notas.push(8);
+notas.push(2);
+notas.push(5);
+
+console.log(notas);
